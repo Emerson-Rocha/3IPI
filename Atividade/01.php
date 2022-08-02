8d885781d4163ce8be89ce1a62ec40b56f77a118
Emerson Rocha <52533818+Emerson-Rocha@users.noreply.github.com>
2022-08-02 10:44:34 -0300

    Primeiros Passo em OOP PHP

---
+++ Atividade/01.php
@@ -0,0 +1,2 @@
+Criar um classe Pessoa com seus atributos, métodos, com visibilidade pública e construtor já inicializado com os valores padrões, caso não seja modificado em sua instância:
+Obs: cria as instância com as etnias.
